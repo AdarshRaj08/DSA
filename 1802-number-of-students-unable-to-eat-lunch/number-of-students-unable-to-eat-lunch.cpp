67b2a2f28252878850a665a9e6a6b2d2f4d94932
Adarsh Raj <132927594+AdarshRaj08@users.noreply.github.com>
2026-04-21 17:48:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (66.07%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int count = 0;
 
-        while(count < students.size())
+        // ************ GOOD APPROACH ****************
+
+        int n = students.size();
+        int arr[2] = {0};
+
+        for(int &stu : students)
         {
-            if(students[0] == sandwiches[0])
+            arr[stu]++;
+        }
+
+        for(int i=0; i<n; i++)
+        {
+            if(arr[sandwiches[i]] > 0)
             {
-                students.erase(students.begin());
-                sandwiches.erase(sandwiches.begin());
-                count = 0;
+                arr[sandwiches[i]]--;
             }
             else
-            {
-                students.push_back(students[0]);
-                students.erase(students.begin());
-                count++;
-            }
+                return n-i;
         }
-        return students.size();
+        return 0;
+
+        // int count = 0;
+
+        // while(count < students.size())
+        // {
+        //     if(students[0] == sandwiches[0])
+        //     {
+        //         students.erase(students.begin());
+        //         sandwiches.erase(sandwiches.begin());
+        //         count = 0;
+        //     }
+        //     else
+        //     {
+        //         students.push_back(students[0]);
+        //         students.erase(students.begin());
+        //         count++;
+        //     }
+        // }
+        // return students.size();
     }
 };
