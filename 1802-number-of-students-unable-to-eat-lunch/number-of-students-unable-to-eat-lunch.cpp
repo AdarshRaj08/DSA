b12990c2695704bed8d037264cab03a86b650b48
Adarsh Raj <132927594+AdarshRaj08@users.noreply.github.com>
2026-04-21 17:27:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (66.07%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int count = 0;
+
+        while(count < students.size())
+        {
+            if(students[0] == sandwiches[0])
+            {
+                students.erase(students.begin());
+                sandwiches.erase(sandwiches.begin());
+                count = 0;
+            }
+            else
+            {
+                students.push_back(students[0]);
+                students.erase(students.begin());
+                count++;
+            }
+        }
+        return students.size();
+    }
+};
