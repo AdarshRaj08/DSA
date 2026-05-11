f908cc216473d0de840f157775e4e7358d6945f8
Adarsh Raj <132927594+AdarshRaj08@users.noreply.github.com>
2026-05-11 12:19:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (49.58%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -20,11 +20,44 @@ public:
         }
         return true;
     }
+
+
+    int countCapital(string word)
+    {
+        int count = 0;
+        for(char &ch : word)
+        {
+            if(ch >= 'A' && ch <= 'Z')
+                count++;
+        }
+        return count;
+    }
+
+
     bool detectCapitalUse(string word) {
-        
-        if(allCapital(word) || allSmall(word) || allSmall(word.substr(1)))
+
+        // ***************** Smart Method **************************
+
+        int n = word.length();
+        int count = countCapital(word);
+
+        if(count == 1)
+        {
+            if(word[0] >= 'A' && word[0] <= 'Z')
+                return true;
+        }
+        if(count == 0 || count == n)
             return true;
         
         return false;
+        
+
+
+        // ***************** Traditional Method **********************
+        
+        // if(allCapital(word) || allSmall(word) || allSmall(word.substr(1)))
+        //     return true;
+        
+        // return false;
     }
 };
